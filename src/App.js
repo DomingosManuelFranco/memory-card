2fafda6f6e0970f28344d0f0b01b791d9adb020f
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-10-07 11:34:40 +0100

    creating the cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import 'bulma/css/bulma.min.css';
+import CardPicture from './CardPicture';
 import ScoreBoard from './ScoreBoard';
 
 function App() {
   return (
     <div className="container">
     <ScoreBoard />
+    <CardPicture />
     </div>
   );
 }
--- src/CardPicture.js
+++ src/CardPicture.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Js from './img/js.png';
+
 
 const CardPicture = () => {
     return (
         <>
-            
+            <div className="card">
+                <div className="card-header">
+                    <div className="card-header-title">
+                        CardPicture
+                    </div>
+                    <div className="card-header-icon">
+                        IconCard
+                    </div>
+                </div>
+                <div className="card-image">
+                    <img src={Js} alt="Js logo"></img>
+                </div>
+                <div className="card-content">
+                    Javascript the Best computer language in the world.
+                </div>
+            </div>
         </>
     )
 }
