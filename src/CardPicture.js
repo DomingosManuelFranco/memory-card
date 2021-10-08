78aab43cf0afbc92e931f9c3f7282d32226fc051
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-10-08 16:38:37 +0100

    styling the card

--- src/CardPicture.js
+++ src/CardPicture.js
@@ -5,14 +5,19 @@ import Js from './img/js.png';
 const CardPicture = () => {
     return (
         <>
-            <div className="container is-max-desktop">
-                <div className="card">
-                   
-                    <div className="card-image">
-                        <img src={Js} alt="Js logo"></img>
-                    </div>
-                    <div className="card-content">
-                        Javascript the Best computer language in the world.
+            <div className="container has-text-centered">
+                <div className="columns is-mobile is-centered">
+                    <div className="column is-2">
+                        <div className="card">
+                            <figure className="image is-square">
+                                <div className="">
+                                    <img src={Js} alt="Js logo"></img>
+                                </div>
+                            </figure>
+                            <div className="card-content">
+                                JS the Best computer language in the world.
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
