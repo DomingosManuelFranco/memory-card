000fc4ba1089ef08fd7adc4df7a0c50653dbaf3d
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-10-26 07:12:57 +0100

    card images  improvements

---
+++ src/Images.js
@@ -0,0 +1,36 @@
+import Angular from './img/angular.png';
+import Csharp from './img/c#.png';
+import Css from './img/css.png';
+import Elixir from './img/elixir.jpeg';
+import Html5 from './img/html.png';
+import Java from './img/java.png';
+import Javascrip from './img/js.png';
+import Kotlin from './img/kotlin.png';
+import Node from './img/node.jpeg';
+import Php from './img/php.png';
+import Python from './img/python.jpeg';
+import Reactjs from './img/react.png';
+import Ruby from './img/ruby.jpeg';
+import Rust from './img/rust.png';
+import Wp from './img/wp.png';
+
+const images = [{language: 'Angular', img: Angular, id: 1}, 
+                {language: 'C#', img: Csharp, id: 2}, 
+                {language: 'Css', img: Css, id: 3}, 
+                {language: 'Elixir', img: Elixir, id: 4}, 
+                {language: 'Html5', img: Html5, id: 5}, 
+                {language: 'Java', img: Java, id: 6}, 
+                {language: 'Javascrip', img: Javascrip, id: 7}, 
+                {language: 'Kotlin', img: Kotlin, id: 8}, 
+                {language: 'Nodejs', img: Node, id: 9}, 
+                {language: 'Php 8', img: Php, id: 10}, 
+                {language: 'Python 3', img: Python, id: 11}, 
+                {language: 'Reactjs', img: Reactjs, id: 12}, 
+                {language: 'Ruby', img: Ruby, id: 13}, 
+                {language: 'Rust', img: Rust, id: 14}, 
+                {language: 'Wordpress', img: Wp, id: 15},
+];
+
+console.log(images.img);
+
+export default images;
