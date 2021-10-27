d556586d42d4bf781979f9be493a2ab774a54294
DomingosManuelFranco <manuelfranco070@gmail.com>
2021-10-27 09:39:24 +0100

    ScoreBoard improvements

--- src/ScoreBoard.js
+++ src/ScoreBoard.js
@@ -4,13 +4,14 @@ const ScoreBoard = () => {
     const [score, setScore] = useState(0)
 
     return (
-        <div className="container mb-6 navbar has-background-link  ">
-            <h2 className="is-size-3 has-text-white">Zoo Memory Game</h2>
-            <div className="navbar-end has-text-white">
-                <h6 className="is-size-4">Score: {score} </h6>
-                <h6 className=" is-size-4 is-bellow">Best Score: {score}</h6>
+        <div className="has-background-link">
+            <div className="container mb-6 navbar has-background-link">
+                <h2 className="is-size-3 has-text-white">Zoo Memory Game</h2>
+                <div className="navbar-end has-text-white">
+                    <h6 className="is-size-4">Score: {score} </h6>
+                    <h6 className=" is-size-4 is-bellow">Best Score: {score}</h6>
+                </div>
             </div>
-            
         </div>
     )
 }
